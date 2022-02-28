7f5eaf795f7a8b6e89e40ea75ed2dd79872f2762
Brendon Ward <brendon.ward@sixlighthouses.com>
2022-02-28 12:44:23 +1000

    add PG and docker-compose

--- app.ts
+++ app.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import { Client } from 'pg';
 
 const app = express();
 const port = 3000;
 
+const client = new Client({
+    password: "root",
+    user: "root",
+    host: "postgres",
+})
+
+
+client.connect();
 
 app.get("/", async (req, res) => {
     res.setHeader("Content-Type", "application/json");
@@ -10,6 +19,24 @@ app.get("/", async (req, res) => {
     res.send({"message": "hello world"});
 })
 
+
+app.get("/employees", async (req, res) => {
+    const results = await client
+      .query("SELECT * FROM employees")
+      .then((payload) => {
+        return payload.rows;
+      })
+      .catch(() => {
+        throw new Error("Query failed");
+      });
+    res.setHeader("Content-Type", "application/json");
+    res.status(200);
+    res.send(JSON.stringify(results));
+  });
+
+    
+  
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 });
+  
